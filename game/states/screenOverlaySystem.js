4b0e269fbeb69a4db43971849489256492da6ad5
Jinni <jinnili2106@gmail.com>
2026-03-09 07:21:58 +0000

    feat: add states folder, screen state scripts, and overlay system

---
+++ game/states/screenOverlaySystem.js
@@ -0,0 +1,31 @@
+import { game } from "../game.js";
+import { drawStartScreen } from "./startScreen.js";
+import { drawIntroScreen } from "./introScreen.js";
+import { drawWinScreen } from "./winScreen.js";
+import { drawLoseScreen } from "./loseScreen.js";
+
+export const screenOverlaySystem = {
+    renderer: null,
+
+    init(renderer) {
+        this.renderer = renderer;
+    },
+
+    afterRender() {
+        if (!this.renderer) return;
+
+        const { ctx, canvas } = this.renderer;
+
+        if (game.state === "playing") return;
+
+        if (game.state === "start") {
+            drawStartScreen(ctx, canvas);
+        } else if (game.state === "intro") {
+            drawIntroScreen(ctx, canvas);
+        } else if (game.state === "win") {
+            drawWinScreen(ctx, canvas);
+        } else if (game.state === "lose") {
+            drawLoseScreen(ctx, canvas);
+        }
+    }
+};
